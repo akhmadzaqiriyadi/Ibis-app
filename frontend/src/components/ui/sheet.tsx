b047796bfb8519325d0fdf39a1b3a9d183a755e2
akhmadzaqir <akhmadzaqiriyadi@gmail.com>
2026-03-05 22:48:17 +0700

    fix(ui): remove dark slate background from sheet component

--- frontend/src/components/ui/sheet.tsx
+++ frontend/src/components/ui/sheet.tsx
@@ -64,13 +64,11 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
-      <div className="flex flex-col h-full bg-slate-900">
-          {children}
-          <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-            <Cross2Icon className="h-4 w-4 text-white" />
-            <span className="sr-only">Close</span>
-          </SheetPrimitive.Close>
-      </div>
+      {children}
+      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+        <Cross2Icon className="h-4 w-4 bg-red-500 rounded text-white" />
+        <span className="sr-only">Close</span>
+      </SheetPrimitive.Close>
     </SheetPrimitive.Content>
   </SheetPortal>
 ))
